4cea4666db42a497a4f1c6399197d2d6c51fb363
Aryan Kumar <aryan.ak.work@gmail.com>
2024-02-05 23:13:42 +0530

    updated url

--- public/js/login.js
+++ public/js/login.js
@@ -8,7 +8,7 @@ export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      url: '/login',
       //   url: 'http://localhost:3000/api/v1/users/login',
       data: {
         email,
